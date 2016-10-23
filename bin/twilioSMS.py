a5f5662e83c60e491c11a1dfd30bb6f39df44466
root <root@ubuntu.members.linode.com>
2016-10-23 05:46:43 +0000

    Finished text recognition response

--- bin/twilioSMS.py
+++ bin/twilioSMS.py
@@ -1,30 +1,38 @@
 from flask import Flask, request, redirect
 import twilio.twiml
+from google import VisionApi
 
 app = Flask(__name__)
-@app.route("/", methods=['GET','POST'])
 
 class SMSRequest:
-   def __init__(self, mediaUrl,analysisType):
-      self.mediaUrl = mediaUrl
-      self.analysisType = analysisType
+    def __init__(self):
+        pass
+    '''def __init__(self, mediaUrl,analysisType):
+        self.mediaUrl = mediaUrl
+        self.analysisType = analysisType
+    '''
+    def getMediaUrl(self):
+        return self.mediaUrl
 
-   def getMediaUrl(self):
-    	return self.mediaUrl
+    def getAnalysisType(self):
+        return self.analysisType
 
-   def getAnalysisType(self):
-   		return self.analysisType
-
+@app.route("/", methods=['GET','POST'])
 def recieveSMSImageAndRespond():
     """Analyze an image sent by a user."""
     # analysisType = request.form['Body']   # Body will contain the analysis the user wants to be done on the image.
     # incoming_number = request.form['From'] 
     # image_url= request.form['MediaUrl0']   # Contains the URL to image which is located on twilio.
 
-    # responseMessage="You chose to analyze your image using: "+ analysisType +"\nStarting image analysis....."
+    vision = VisionApi()
+    filename = vision.writeFile(url=request.form['MediaUrl0'])
+    print filename
+    responseMessage = vision.detectText(input_filenames=[filename])
 
     # Call a functon to analyze the image. It returns a string which contains the analysis.
-    analysisResult=#myfunctioncall(Request(request.form['From'],request.form['MediaUrl0']))
+    analysisResult=responseMessage[filename][0]['description']
+    print "Analysis result:"
+    print analysisResult
 
     # return the analysis back to the sender.
     resp = twilio.twiml.Response()
@@ -37,4 +45,4 @@ def recieveSMSImageAndRespond():
     return str(resp)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
